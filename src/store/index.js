d1ae49fa94c67ad5056a6b7bf3bdb98ad7531e7a
jjmka407 <jjmka407@gmail.com>
2021-10-23 03:56:04 +0000

    Prepare store to consume API and upload table data(state products)

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: [],
   },
   mutations: {
+    setProducts(state, payload) {
+      state.products = payload;
+    },
   },
   actions: {
+    async getProducts({ commit }) {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/products-filter/`
+        );
+        if (response.statusText == "OK") {
+          commit("setProducts", response.data);
+          
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+    },
   },
   modules: {
   }
