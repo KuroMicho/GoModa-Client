4128123931d363cfc336fbbbc8ad93f722e5d5e2
Toya-Vargas <toyavaroa@gmail.com>
2021-10-26 22:49:02 -0500

    Include Table component

---
+++ src/components/Table.vue
@@ -0,0 +1,40 @@
+<template>
+    <table class="highlight centered responsive-table">
+        <thead>
+          <tr>
+              <th>Barcode</th>
+              <th>Name</th>
+              <th>Description</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td> </td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+          </tr>
+        </tbody>
+      </table>
+        
+</template>
+
+<script>
+export default {
+    name: 'Table'
+
+}
+</script>
+
+<style scoped>
+</style>
